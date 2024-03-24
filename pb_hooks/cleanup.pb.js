02093eefc7d030170a1c0ad3455b92c396bf122a
JT Archie <jtarchie@gmail.com>
2024-03-24 04:53:30 -0600

    cleanup old boards

---
+++ pb_hooks/cleanup.pb.js
@@ -0,0 +1,32 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+cronAdd("cleanup unused boards", "0 0 * * 0", () => {
+  const ago = new Date(
+    new Date().getTime() - (7 * 24 * 60 * 60 * 1000),
+  );
+
+  const boards = $app.dao().findRecordsByFilter(
+    "boards", // collection
+    "created < {:ago}", // filter
+    "", // sort
+    0, // limit
+    0, // offset
+    { ago: ago.toISOString() }, // optional filter params
+  );
+
+  boards.forEach((record) => {
+    const items = $app.dao().findRecordsByFilter(
+      "items", // collection
+      "board_id = {:board_id}", // filter
+      "", // sort
+      1, // limit
+      0, // offset
+      { board_id: record.id }, // optional filter params
+    );
+
+    if (items.length == 0) {
+      console.log("record", record.id, record.getCreated());
+      $app.dao().deleteRecord(record);
+    }
+  });
+});
